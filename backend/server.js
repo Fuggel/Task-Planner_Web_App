f23de9b4250e8755b5d5dd3cb3e0a90c3af19a19
Fuggel <furkanserdarceylan@gmail.com>
2023-01-21 15:13:15 +0100

    init heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
-require("dotenv").config();
+require("dotenv").config({ path: "../.env" });
 
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
@@ -16,6 +17,15 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/todos", require("./routes/todoRoutes"));
 app.use("/users", require("./routes/userRoutes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.use(errorHandler);
 
